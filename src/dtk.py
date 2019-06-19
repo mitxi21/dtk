f44840ca99cba09f7233236dbd957968840bab57
Miguel Perales <miguel.peralesbermejo@iqvia.com>
2019-06-19 11:21:35 +0200

    Single instances using wx

--- src/dtk.py
+++ src/dtk.py
@@ -236,64 +236,28 @@ class EditSettingsFrame(wx.Dialog):
     def OnCancel(self, e):
         self.Close(True)
 
+class SingleApp(wx.App):
+    """
+    class that extends wx.App and only permits a single running instance.
+    """
 
-
-# This method checks if file pid_file exists.
-#  If it was found, depends of mode - exit, or kill process which PID is stored in file.
-def scriptStarter(mode='force'):
-    '''
-        if mode  = force - kill runing script and run new one
-        if mode != force - run script only if it's not already running
-    '''
-    if os.path.exists(pid_file):
-        print
-        'old copy found'
-        if mode == 'force':
-            print
-            'running copy found, killing it'
-            # reading PID from file and convert it to int
-            pid = int((open(pid_file).read()))
-            print
-            'pid have been read:', pid
-            # If you use pythton 2.7 or above just use os.kill to terminate PID process.
-            # In case of python 2.6 run method killing selected PID
-            kill(pid)
-
-        else:
-            print
-            'running copy found, leaving'
-            # not force mode. Just don't run new copy. Leaving.
-            raise SystemExit
-
-            # If we are here, mode == force, old copy killed, writing PID for new script process
-    open(pid_file, 'w').write(str(os.getpid()))
-
-
-def kill(pid):
-    """kill function for Win32"""
-    kernel32 = ctypes.windll.kernel32
-    handle = kernel32.OpenProcess(1, 0, pid)
-    return (0 != kernel32.TerminateProcess(handle, 0))
-
-
-# Delete pid-file befor script finish.
-# Dont forget to run this method at the end of the script
-# If you use PyQt call it in overloaded closeEvent()
-def removePIDfile():
-    print
-    'deleting pidfile'
-    try:
-        os.remove(pid_file)
-    except OSError:
-        pass
-    return
+    def OnInit(self):
+        """
+        wx.App init function that returns False if the app is already running.
+        """
+        self.name = "SingleApp-%s".format(wx.GetUserId())
+        self.instance = wx.SingleInstanceChecker(self.name)
+        if self.instance.IsAnotherRunning():
+            wx.MessageBox(
+                "An instance of the application is already running",
+                "Error",
+                 wx.OK | wx.ICON_WARNING
+            )
+            return False
+        return True
 
 if __name__ == '__main__':
-    # check if script is already running
-    scriptStarter('force')
-    app = wx.App(False)
+    app = SingleApp(redirect=False)
     frame = MainFrame(None, title='DTK')
     app.MainLoop()
-    # remove pid-file before exit.
-    removePIDfile()
 
